f673dc77cf3188fa92ab68f1b41606f8b83f5207
Habibur Rahman <Habibur@Habiburs-Mac-mini.local>
2017-03-10 01:34:42 +0600

    functionality  and launch screen added

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,18 +7,154 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var buttonSound: AVAudioPlayer!
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    
+    
+    enum Operation: String {
+        
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    var currentOperation = Operation.Empty
+    var runningNumber = ""
+    var rightValStr = ""
+    var LeftValStr = ""
+    var result = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do{
+            try buttonSound = AVAudioPlayer(contentsOf: soundURL)
+            buttonSound.prepareToPlay()
+            
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        
+        outputLabel.text = "0"
+    }
+    
+    
+    
+    @IBAction func buttonPressed(sender: UIButton){
+        
+       playSound()
+        
+        
+        runningNumber += "\(sender.tag)"
+        
+        outputLabel.text = runningNumber
+        
+        
+        
+    }
+    
+    func playSound(){
+        
+        if buttonSound.isPlaying {
+            buttonSound.stop()
+        }
+        
+        buttonSound.play()
+        
+    }
+    
+    
+    
+    @IBAction func onDividePressd(sender: AnyObject){
+        
+        processOperation(operation: .Divide)
+    }
+    
+    @IBAction func onMultiplyPressd(sender: AnyObject){
+        
+        processOperation(operation: .Multiply)
+    }
+    
+    @IBAction func onAddPressd(sender: AnyObject){
+        
+        processOperation(operation: .Add)
+    }
+    
+    @IBAction func onSubtractPressd(sender: AnyObject){
+        
+        processOperation(operation: .Subtract)
+    }
+    
+    
+    @IBAction func onEqualPressed(sender: AnyObject){
+        
+        processOperation(operation: currentOperation)
+        
+        
+    }
+    
+    func processOperation(operation: Operation) {
+        
+        
+        if currentOperation != Operation.Empty{
+            
+            // A user selected an operator , and then selected another operator without first entering a number
+            
+            if runningNumber != ""{
+                
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Divide {
+                    
+                    result = "\(Double(LeftValStr)! / Double(rightValStr)!)"
+                    
+                }else if currentOperation == Operation.Multiply {
+                    
+                    result = "\(Double(LeftValStr)! * Double(rightValStr)!)"
+                    
+                }else if currentOperation == Operation.Add {
+                    
+                    result = "\(Double(LeftValStr)! + Double(rightValStr)!)"
+                    
+                }else if currentOperation == Operation.Subtract {
+                    
+                    result = "\(Double(LeftValStr)! - Double(rightValStr)!)"
+                }
+                
+                
+                LeftValStr = result
+                outputLabel.text = LeftValStr
+                
+                }
+            
+            currentOperation = operation
+            
+        }else {
+            
+            // This is the first time an operator has been pressed
+            
+            LeftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
